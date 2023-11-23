d8d54f95c8a43b528eefb8411846eab402272933
hoangnam714 <51524705+hoangnam714@users.noreply.github.com>
2023-11-23 09:35:00 +0700

    Add files via upload

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by admin on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
